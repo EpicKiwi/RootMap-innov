963b8322d093b96c491db455fae2d5c8bcc27453
EpicKiwi <me@epickiwi.fr>
2019-11-21 13:02:36 +0100

    Support odb

--- compass/compass.py
+++ compass/compass.py
@@ -3,6 +3,7 @@ import time
 import serial
 import sys
 import math
+import obd
 
 if len(sys.argv) != 2:
 	print("Usage example : python3 compass.py /dev/ttyACM1")
@@ -14,8 +15,11 @@ def get_time():
 
 
 def get_speed():
-	return 8
-
+	if connection.status() == obd.OBDStatus.CAR_CONNECTED :
+		speed = connection.query(obd.commands.SPEED).value
+		return speed.magnitude / 3.6
+	else:
+		return 8
 
 def compute_heading(x, y, z, declination_angle=-0.01745329, in_degree=False):
 	angle = math.atan2(y, x)
@@ -28,13 +32,20 @@ def compute_heading(x, y, z, declination_angle=-0.01745329, in_degree=False):
 		angle = angle * 180/math.pi
 	return angle
 
+# obd.logger.setLevel(obd.logging.DEBUG)
+
+connection = obd.OBD()
+
+if connection.status() != obd.OBDStatus.CAR_CONNECTED:
+	print("ODB Adapter not connected, using constant speed of 8 m/s", file=sys.stderr)
+
 serial_port = sys.argv[1]
 serial_compass = serial.Serial(serial_port, timeout=1)
 last_pos = (0, 0)
 last_point_time = get_time()
 full_distance = 0
 time_elapsed = 0
-
+slowmode = False
 next_write = 0
 
 try:
@@ -82,12 +93,14 @@ try:
 		last_pos = current_pos
 		last_point_time = current_time
 
-		if next_write < time_elapsed:
+		if slowmode == True and next_write < time_elapsed:
 			print("{};{};{}".format(
 				current_pos[0],
 				current_pos[1],
 				speed), flush=True)
 			next_write = time_elapsed + 0.05
+
+		serial_compass.flushInput()
 
 except KeyboardInterrupt:
 	exit(0)
--- obd-analyzer/obd-analyzer.py
+++ obd-analyzer/obd-analyzer.py
@@ -1,9 +1,9 @@
 from time import sleep
 import obd
 
-obd.logger.setLevel(obd.logging.DEBUG)
+# obd.logger.setLevel(obd.logging.DEBUG)
 
-connection = obd.OBD(baudrate=9600)
+connection = obd.OBD(fast=False, check_voltage=False, timeout=1)
 
 if connection.status() != obd.OBDStatus.CAR_CONNECTED:
 	print("Adapter not connected")
@@ -12,9 +12,8 @@ if connection.status() != obd.OBDStatus.CAR_CONNECTED:
 while 1:
 	speed_res = connection.query(obd.commands.SPEED)
 	rpm_res = connection.query(obd.commands.RPM)
-	temp_res = connection.query(obd.commands.OIL_TEMP)
-	accel_pos = connection.query(obd.commands.RELATIVE_ACCEL_POS)
 
-	print("{} %, {} rpm, {} Km/h, {}°C".format(accel_pos.value, rpm_res.value, speed_res.value, temp_res.value))
+	rpm = rpm_res.value
+	speed = speed_res.value
 
-	sleep(0.1)
+	print("{}, {}".format(rpm, speed))
